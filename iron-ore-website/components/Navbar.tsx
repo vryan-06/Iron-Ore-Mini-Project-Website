e14e82ba877b0eb9739770ff37adb1a673d3344e
deon-gracias <deongracias1@gmail.com>
2023-04-23 18:46:27 +0530

    Change in navbar text

--- iron-ore-website/components/Navbar.tsx
+++ iron-ore-website/components/Navbar.tsx
@@ -1,16 +1,15 @@
-import { Disclosure } from '@headlessui/react'
-import { Menu, X } from 'heroicons-react'
-import Link from 'next/link'
+import { Disclosure } from "@headlessui/react";
+import { Menu, X } from "heroicons-react";
+import Link from "next/link";
 
 const navigation = [
-  { name: 'Home', href: '/', current: true },
-  { name: 'Find an Iron Mine', href: '/buyer', current: false },
-  { name: 'Sell Iron Ore', href: '/prediction', current: false },
-
-]
+  { name: "Home", href: "/", current: true },
+  { name: "Find an Iron Mine", href: "/buyer", current: false },
+  { name: "Sell Iron Ore", href: "/prediction", current: false },
+];
 
 function classNames(...classes: string[]) {
-  return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(" ");
 }
 
 export default function Navbar() {
@@ -45,11 +44,11 @@ export default function Navbar() {
                         href={item.href}
                         className={classNames(
                           item.current
-                            ? 'bg-green-500 text-white shadow-lg'
-                            : 'text-slate-700 hover:bg-green-500 hover:text-white hover:shadow-lg',
-                          'rounded-md px-3 py-2 text-sm font-medium'
+                            ? "bg-green-500 text-white shadow-lg"
+                            : "text-slate-700 hover:bg-green-500 hover:text-white hover:shadow-lg",
+                          "rounded-md px-3 py-2 text-sm font-medium"
                         )}
-                        aria-current={item.current ? 'page' : undefined}
+                        aria-current={item.current ? "page" : undefined}
                       >
                         {item.name}
                       </a>
@@ -58,10 +57,10 @@ export default function Navbar() {
                 </div>
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <button className="rounded-md border border-green-500 px-3 py-2 text-sm font-medium text-white hover:bg-green-500 hover:text-white  hover:shadow-lg">
+                <button className="rounded-md border border-green-500 px-3 py-2 text-sm font-medium text-green-500 hover:bg-green-500 hover:text-white  hover:shadow-lg">
                   <Link href="/signup">Register</Link>
                 </button>
-                <button className="ml-4 rounded-md border border-green-500 px-3 py-2 text-sm font-medium text-white hover:bg-green-500 hover:text-white  hover:shadow-lg">
+                <button className="ml-4 rounded-md border border-green-500 px-3 py-2 text-sm font-medium text-green-500 hover:bg-green-500 hover:text-white  hover:shadow-lg">
                   <Link href="/login">Login</Link>
                 </button>
               </div>
@@ -77,11 +76,11 @@ export default function Navbar() {
                   href={item.href}
                   className={classNames(
                     item.current
-                      ? 'bg-green-500 text-white shadow-lg'
-                      : 'text-slate-700 hover:bg-green-500 hover:text-white hover:shadow-lg',
-                    'block rounded-md px-3 py-2 text-base font-medium'
+                      ? "bg-green-500 text-white shadow-lg"
+                      : "text-slate-700 hover:bg-green-500 hover:text-white hover:shadow-lg",
+                    "block rounded-md px-3 py-2 text-base font-medium"
                   )}
-                  aria-current={item.current ? 'page' : undefined}
+                  aria-current={item.current ? "page" : undefined}
                 >
                   {item.name}
                 </Disclosure.Button>
@@ -91,5 +90,5 @@ export default function Navbar() {
         </>
       )}
     </Disclosure>
-  )
+  );
 }
