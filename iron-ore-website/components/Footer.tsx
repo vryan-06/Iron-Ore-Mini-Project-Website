2dcf48e314e20644e5ef343c547a0c209f0cb2c0
V Ryan <73381366+vryan-06@users.noreply.github.com>
2023-07-28 16:59:34 +0530

    Update Footer.tsx

--- iron-ore-website/components/Footer.tsx
+++ iron-ore-website/components/Footer.tsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <div className="flex flex-col items-center justify-around bg-white p-3">
       <h1 className="text-base font-semibold text-gray-800">
-        © All rights reserved | Built by
+        © All rights reserved | Built by{' '}
         <span className="cursor-pointer font-semibold hover:text-green-600">
           Ryan V and Deon Gracias
         </span>
